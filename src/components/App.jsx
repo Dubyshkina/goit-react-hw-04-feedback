5d7dbf5a9799563bd459725a870e481d68a2dc33
Dubyshkina <ek.kalyazina@gmail.com>
2023-05-09 11:51:59 +0300

    fixed feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,13 +20,13 @@ const App = () => {
   const onLeaveFeedback = e => {
     switch (e.target.id) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       default:
         console.log('Unknown id');
